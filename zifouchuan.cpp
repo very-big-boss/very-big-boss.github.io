1672e30d396dbb859042b51b43f7d9bc8400edfe
very-big-boss <72069824+very-big-boss@users.noreply.github.com>
2020-10-23 13:05:27 +0800

    Create zifouchuan.cpp

---
+++ zifouchuan.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+char a[50];
+
+int main()
+{
+    cin>>a;
+    for (int i=0;a[i]!='\0';i++){
+        cout<<a[i]<<" ";
+    }
+    return 0;
+}
