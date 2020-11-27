d5853c8a57a91f0ccb84f051f1a7ad37e9fda6f2
very-big-boss <72069824+very-big-boss@users.noreply.github.com>
2020-11-27 16:26:00 +0800

    Update 26.cpp

--- 26.cpp
+++ 26.cpp
@@ -1,56 +1,28 @@
+
 #include<iostream>
 #include<string>
 #include<algorithm>
-#include<stdlib.h>
+#include<sstream>
 using namespace std;
-string h2o(string a)
+int h2o(string a)
 {
-	if(a=="0"||a=="-0")
-	{
-		return "0";
-	}
+	bool k=0;
+	if(a[0]=='-')
+		k=1;
+	reverse(a.begin(), a.end());
+	stringstream ss;int h;
+	ss<<a;ss>>h;ss.clear();
+	if(k==0)
+		return h;
 	else
-	{
-		if(a[0]=='-')
-		{
-			a[0]='\0';a=a+'-';
-		}
-		reverse(a.begin(), a.end());
-		for(int i=0;i<=a.size();i++)
-		{
-			if(a[i]=='0')
-			{
-				a[i]='\0';
-			}
-			if(a[i]!='0'&&a[i]!='\0'&&a[i]!='-')
-			{
-				break;
-			}
-		}
-		return a;
-	}
+		return -h;
 }
 int main()
 {
 	string a[6];
 	for(int i=0;i<=5;i++)
-	{
 		cin>>a[i];
-	}
 	for(int i=0;i<=5;i++)
-	{
-		string k=h2o(a[i]);
-		char l[100]={'\0'};
-		for(int i=0,u=0;i<=k.size()-1;i++)
-		{
-			if(k[i]=='\0')
-			{
-				continue;
-			}
-			l[u]=k[i];u+=1;
-		}
-		int g=atoi(l);
-		cout<<g<<endl;
-	}
+		cout<<h2o(a[i])<<endl;
 	return 0;
 }
